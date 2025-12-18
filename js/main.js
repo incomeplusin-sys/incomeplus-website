ffbe442a301af77e51471bfb44febc057f319839
incomeplusin-sys <incomeplus.in@gmail.com>
2025-12-18 13:52:30 +0530

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// Add stock scanner to mobile menu
+const mobileMenuHTML = `
+    <a href="/" class="block py-3 text-gray-700 hover:text-blue-600">Home</a>
+    <a href="scanner.html" class="block py-3 text-gray-700 hover:text-blue-600">Security Scanner</a>
+    <a href="stockscanner.html" class="block py-3 text-gray-700 hover:text-blue-600">Stock Scanner</a>
+    <a href="pricing.html" class="block py-3 text-gray-700 hover:text-blue-600">Pricing</a>
+    <a href="blog/" class="block py-3 text-gray-700 hover:text-blue-600">Blog</a>
+`;
